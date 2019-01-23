bf6400f6a413fb1696c4acaee284d46f23b2f920
Sean Trinh <strinh@stevens.edu>
2019-01-23 14:13:20 -0500

    init for lab1

---
+++ lab1/lab1.js
@@ -0,0 +1,28 @@
+//Lab 1 - CS 546
+//I pledge my honor that I have abided by the Stevens Honor System.
+
+const questionOne = function questionOne(arr) {
+    // Implement question 1 here
+}
+
+const questionTwo = function questionTwo(num) { 
+    // Implement question 2 here
+}
+
+const questionThree = function questionThree(text) {
+    // Implement question 3 here
+}
+
+const questionFour = function questionFour(num) {
+    // Implement question 4 here
+}
+
+module.exports = {
+    firstName: "Sean", 
+    lastName: "Trinh", 
+    studentId: "10416232",
+    questionOne,
+    questionTwo,
+    questionThree,
+    questionFour
+};
