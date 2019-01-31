aefece454b1c50f2753d6edeee5ad04d40c88678
Sean Trinh <strinh@stevens.edu>
2019-01-30 22:29:10 -0500

    function declarations for arrayutils

--- lab2/arrayUtils.js
+++ lab2/arrayUtils.js
@@ -1,2 +1,31 @@
 //Lab 2 - CS 546
 //I pledge my honor that I have abided by the Stevens Honor System.
+
+const head = function head(arr) {
+        //Returns the head of the array
+        
+}
+
+const last = function last(arr) {
+	//Returns the last element of the array
+
+}
+
+const remove = function remove(arr,index) {
+	//Removes the element at the specified index
+
+}
+
+const range(end,value) = function range(end,value) {
+	
+}
+
+const countElements = function countElements(arr) {
+
+}
+
+const isEqual = function isEqual(arrayOne,arrayTwo) {
+
+}
+
+
