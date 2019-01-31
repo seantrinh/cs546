7ab93cd5fd76081c093d7748977ea095c3d02f3f
Sean Trinh <strinh@stevens.edu>
2019-01-31 13:56:45 -0500

    finished test cases

--- lab2/index.js
+++ lab2/index.js
@@ -34,28 +34,32 @@ try {
 
 //Remove Tests
 try {
-
+	const removeOne = arrayUtils.remove([5,6,7],1);
+	console.log('Remove passed SUCCESSFULLY');
 } catch (e) {
-
+	console.error('Remove FAILED test case');
 }
 
 try {
-
+	const removeTwo = arrayUtils.remove([5,6,7],-1);
+	console.error('Remove DID NOT ERROR');
 } catch (e) {
-
+	console.log('Removed failed SUCCESSFULLY');
 }
 
 //Range Tests
 try {
-
+	const rangeOne = arrayUtils.range(4);
+	console.log('Range passed SUCCESSFULLY');
 } catch (e) {
-
+	console.error('Range FAILED test case');
 }
 
 try {
-
+	const rangeTwo = arrayUtils.range(-1);
+	console.error('Range DID NOT ERROR');
 } catch (e) {
-
+	console.log('Range failed SUCCESSFULLY');
 }
 
 
