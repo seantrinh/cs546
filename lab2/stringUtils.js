58f4f26d126fba0dfe01e79daba2f4ea8ca454e7
Sean Trinh <strinh@stevens.edu>
2019-01-31 11:48:45 -0500

    added func declarations

--- lab2/stringUtils.js
+++ lab2/stringUtils.js
@@ -1,2 +1,14 @@
 //Lab 2 - CS 546
 //I pledge my honor that I have abided by the Stevens Honor System.
+
+const capitalize = function capitalize(string) {
+	
+}
+
+const repeat = function repeat(string, num) {
+
+}
+
+const countChars = function countChars(string) {
+
+}
