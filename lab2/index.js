fd55b153b56ccbb2a85558c4563dc3a99631b288
Sean Trinh <strinh@stevens.edu>
2019-01-31 13:34:09 -0500

    provided skeleton for tests

--- lab2/index.js
+++ lab2/index.js
@@ -1,2 +1,57 @@
 //Lab 2 - CS 546
 //I pledge my honor that I have abided by the Stevens Honor System.
+
+//Head Tests
+try {
+
+} catch (e) {
+
+}
+
+try {
+
+} catch (e) {
+
+}
+
+//Last Tests
+try {
+
+} catch (e) {
+
+}
+
+try {
+
+} catch (e) {
+
+}
+
+//Remove Tests
+try {
+
+} catch (e) {
+
+}
+
+try {
+
+} catch (e) {
+
+}
+
+//Range Tests
+try {
+
+} catch (e) {
+
+}
+
+try {
+
+} catch (e) {
+
+}
+
+
+
