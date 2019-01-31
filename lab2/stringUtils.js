4ee5850bbdde2a3451ae6e20f675c6b07bc53ca6
Sean Trinh <strinh@stevens.edu>
2019-01-31 11:58:37 -0500

    completed capitalize func

--- lab2/stringUtils.js
+++ lab2/stringUtils.js
@@ -2,7 +2,13 @@
 //I pledge my honor that I have abided by the Stevens Honor System.
 
 const capitalize = function capitalize(string) {
-	
+	if (typeof string != "string" || string === undefined) {
+		throw "Not a valid string!";
+	}
+	else {
+		if (string == "") { return ""; }
+		return string[0].toUpperCase() + string.slice(1).toLowerCase();
+	}
 }
 
 const repeat = function repeat(string, num) {
