3d8bb07050671d1b982be02b3befdb6387d1c348
Sean Trinh <strinh@stevens.edu>
2019-01-31 12:28:59 -0500

    added func declarations

--- lab2/objUtils.js
+++ lab2/objUtils.js
@@ -1,2 +1,14 @@
 //Lab 2 - CS 546
 //I pledge my honor that I have abided by the Stevens Honor System.
+
+const extend = function extend(...args) {
+
+}
+
+const smush = function smush(...args) {
+
+}
+
+const mapValues = function mapValues(object, func) {
+
+}
