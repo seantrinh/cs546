734e67c6116dbc31172c02be9211314c07a0b8b9
Sean Trinh <strinh@stevens.edu>
2019-02-10 01:19:08 -0500

    headway on getPersonById

--- lab3/functions.js
+++ lab3/functions.js
@@ -1,8 +1,21 @@
 // Lab 3 - CS 546
 // I pledge my honor that I have abided by the Stevens Honor System.
+const axios = require('axios');
 
-const getPersonById = function getPersonById(index) {
+const getPeople = async function getPeople() {
+	return await axios.get('https://gist.githubusercontent.com/robherley/5112d73f5c69a632ef3ae9b7b3073f78/raw/24a7e1453e65a26a8aa12cd0fb266ed9679816aa/people.json');
+}
 
+const getPersonById = async function getPersonById(index) {
+	if (isNaN(index) || !Number.isInteger(index) || index === undefined) {
+		throw "Integer not provided!";
+	}
+	if (index < 0) { throw "Index out of bounds!"; }
+	data = await getPeople();
+	if (index >= data.data.length) {
+		throw "Index out of bounds!";
+	}
+	return data.data[index].firstName + " " + data.data[index].lastName;
 }
 
 const lexIndex = function lexIndex(index) {
@@ -24,3 +37,10 @@ const whereDoTheyWork = function whereDoTheyWork(firstName, lastName) {
 const findTheHacker = function findTheHacker(ip) {
 
 }
+
+let test = getPersonById(42);
+console.log(test);
+
+test.then(function(result) {
+	console.log(test);
+});
