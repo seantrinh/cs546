2948d6885702116ab9698484d398e83fe5ec6dbe
Sean Trinh <strinh@stevens.edu>
2019-02-26 10:20:09 -0500

    changes

--- lab4/data/animals.js
+++ lab4/data/animals.js
@@ -3,6 +3,7 @@
 
 const mongoCollections = require("../mongoCollections");
 const animals = mongoCollections.animals;
+const {ObjectId} = require('mongodb');
 
 module.exports = {
 	async create(name, animalType) {
@@ -24,7 +25,7 @@ module.exports = {
 		}
 		const newId = insertInfo.insertedId;
 		
-		const animal = await this.get(newId);
+		const animal = await this.get(String(newId));
 		return animal;
 	},
 	async getAll() {
@@ -33,23 +34,23 @@ module.exports = {
 		return animal_array;
 	},
 	async get(id) {
-		if (!id) {
+		if (!id || typeof id !== 'string') {
 			throw "You must provide an id to search for!";
 		}	
 		const animalCollection = await animals();
-		const animal = await animalCollection.findOne({ _id: id });
+		const animal = await animalCollection.findOne({ _id: ObjectId(id) });
 		if (animal === null) {
 			throw "No animal with that id!";
 		}
 		return animal;
 	},
 	async remove(id) {
-		if (!id) {
+		if (!id || typeof id !== 'string') {
 			throw "You must provide an id to search for!";
 		}
 		const animalCollection = await animals();
 		const removedAnimal = await this.get(id);
-		const deletionInfo = await animalCollection.removeOne({ _id: id });
+		const deletionInfo = await animalCollection.removeOne({ _id: ObjectId(id) });
 		if (deletionInfo.deletedCount === 0) {
 			throw `Could not delete animal with id of ${id}`;
 		}
@@ -60,7 +61,7 @@ module.exports = {
 		return rAnimal;
 	},
 	async rename(id, newName) {
-		if (!id) {
+		if (!id || typeof id !== 'string') {
 			throw "You must provide an id to search for!";
 		}
 		if (!newName || typeof newName !== "string") {
@@ -73,16 +74,16 @@ module.exports = {
 			name: newName,
 			animalType: anType
 		};
-		const updateInfo = await animalCollection.updateOne({ _id: id }, { $set: updatedAnimal });
+		const updateInfo = await animalCollection.updateOne({ _id: ObjectId(id) }, { $set: updatedAnimal });
 		if (updateInfo.modifiedCount === 0) {
 			throw "Could not update dog successfully!";	
 		}
 		return await this.get(id);
+	},
+	async removeAll() {
+		//JUST FOR TESTING PURPOSES
+		const animalCollection = await animals();
+		const deletionInfo = await animalCollection.deleteMany({});
+		return deletionInfo;
 	}
-	//async removeAll() {
-	//	//JUST FOR TESTING PURPOSES
-	//	const animalCollection = await animals();
-	//	const deletionInfo = await animalCollection.deleteMany({});
-	//	return deletionInfo;
-	//}
 };
