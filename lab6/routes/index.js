1a82b52927f1d2839e51988179fb6a2a8d00ac21
Sean Trinh <strinh@stevens.edu>
2019-03-02 20:50:33 -0500

    index

--- lab6/routes/index.js
+++ lab6/routes/index.js
@@ -1,2 +1,15 @@
 // Lab 6 - CS 546
 // I pledge my honor that I have abided by the Stevens Honor System.
+const aboutRoutes = require("./about");
+const educationRoutes = require("./education");
+const storyRoutes = require("./story");
+
+const constructorMethod = app => {
+	app.use("/about",aboutRoutes);
+	app.use("/education",educationRoutes);
+	app.use("/story",storyRoutes);
+
+	app.use("*", (req,res) => {
+		res.status(404).json({ error: "Not found!" });
+	});
+};
