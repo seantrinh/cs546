3fb38c4176791f7d93ac13ea0394a672548b5df5
Sean Trinh <strinh@stevens.edu>
2019-03-11 18:12:21 -0400

    skeleton for posts

--- lab7/data/posts.js
+++ lab7/data/posts.js
@@ -1,2 +1,25 @@
 // CS 546 - Lab 7
 // I pledge my honor that I have abided by the Stevens Honor System.
+const mongoCollections = require("./collections");
+const posts = mongoCollections.posts;
+
+module.exports = {
+	async getPostById(id) {
+
+	},
+	async addPost(title, author, content) {
+
+	},
+	async updatePost(id, title, author, content) {
+
+	},
+	async deletePost(id) {
+
+	},
+	async removeAll() {
+		// JUST FOR TESTING PURPOSES
+		const postCollection = await posts();
+                const deletionInfo = await postCollection.deleteMany({});
+                return deletionInfo;
+	}
+};
