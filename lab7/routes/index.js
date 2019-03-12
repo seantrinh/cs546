6d33d6a9b8cc6caf73faead92128a33bccd87bd2
Sean Trinh <strinh@stevens.edu>
2019-03-11 20:24:05 -0400

    index for routes

--- lab7/routes/index.js
+++ lab7/routes/index.js
@@ -1,2 +1,17 @@
 // CS 546 - Lab 7
 // I pledge my honor that I have abided by the Stevens Honor System.
+const animalRoutes = require("./animals");
+const postRoutes = require("./posts");
+const likeRoutes = require("./likes");
+
+const constructorMethod = app => {
+	app.use("/animals", animalRoutes);
+	app.use("/posts", postRoutes);
+	app.use("/likes", likeRoutes);
+
+	app.use("*", (req, res) => {
+		res.status(404).json({ error: "Not found!" });
+	});
+});
+
+module.exports = constructorMethod;
